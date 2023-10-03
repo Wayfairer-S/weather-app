c6b524285def6b5eb7be77ca377acef1761482a0
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-10-03 21:21:07 +0100

    Passing snapshot.

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
 import { render } from "@testing-library/react";
 import ForecastSummary from '../../components/ForecastSummary';
 
@@ -25,5 +26,22 @@ import ForecastSummary from '../../components/ForecastSummary';
         );
     
         expect(asFragment()).toMatchSnapshot();
+      });
+      it("render correct values for props", () => {
+        const { getByText, getByTestId } = render(
+          <ForecastSummary
+            date={validProps.date}
+            description={validProps.description}
+            icon={validProps.icon}
+            temperature={validProps.temperature}
+          />
+        );
+    
+        expect(getByText("1111111")).toHaveClass("forecast-summary__date");
+        expect(getByText("Stub description")).toHaveClass(
+          "forecast-summary__description"
+        );
+        expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary__icon");
+        expect(getByText("22°C")).toHaveClass("forecast-summary__temperature");
       });
     });
