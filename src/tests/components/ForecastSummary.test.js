2caa68582aca247225c6774e59a29e24e8b1f51d
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-10-03 21:14:44 +0100

    Added snapshot testing to the Forecast.js

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -5,11 +5,25 @@ import ForecastSummary from '../../components/ForecastSummary';
 
 
     describe("ForecastSummary", () => {
-        it("renders a forecast summary", () => {
-          const { getByText } = render(
-            <ForecastSummary date="1525046400000" temperature="11°c" description="Clear" icon="211"/>
-          );
-           // eslint-disable-next-line testing-library/prefer-screen-queries
-           expect(getByText(/1525046400000/i, /11°c/i, /Clear/i, /211/i)).toBeTruthy();
-        });
+      const validProps = {
+        date: 1111111,
+        description: "Stub description",
+        icon: "stubIcon",
+        temperature: {
+          min: 12,
+          max: 22,
+        },
+      };
+      it("renders correctly", () => {
+        const { asFragment } = render(
+          <ForecastSummary
+            date={validProps.date}
+            description={validProps.description}
+            icon={validProps.icon}
+            temperature={validProps.temperature}
+          />
+        );
+    
+        expect(asFragment()).toMatchSnapshot();
       });
+    });
